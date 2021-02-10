86f8298a9a4765b5e0251d82182e3bc8f7ca7217
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 17:41:31 -0300

    Aula de v-once concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <h1>Hello World</h1>
+  <!-- A diretiva v-once, impede que a associação de dados seja reativa onde ele for utilizada -->
+  <p v-once>Estoque Inicial: {{ stock }}</p>
+  <p>Estoque Atualizado: {{ stock }}</p>
+
+  <button @click="stock++">Aumentar Estoque</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    stock: 1
+  })
+})
 </script>
 
 <style></style>
