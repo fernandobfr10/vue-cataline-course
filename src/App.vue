43deb4d748fe3ce244e5bf9c8dd42c63919d6bf0
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 17:12:00 -0300

    Aula de v-text concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <p>{{ company }} é legal</p>
+
+    <!-- Utilizando v-text o conteúdo vindo de data pega o lugar do conteúdo que a tag receberia, não sendo possível concatenar como fiz acima -->
+    <p v-text="company"></p>
+
+    <!-- Irá dar erro -->
+    <p v-text="company">é legal</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    company: 'Cataline'
+  })
+})
 </script>
 
 <style></style>
