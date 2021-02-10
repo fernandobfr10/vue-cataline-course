2ee53f84294879c09c83e7783a944c7b3c46acea
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 17:05:40 -0300

    Aula de v-bind concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1>Hello World</h1>
+  <a :href="product.url" :class="productClass">
+    {{ product.name }}
+  </a>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    product: {
+      name: 'Camisa',
+      url: 'http://loja.com/produtos/12345',
+      stock: true
+    }
+  }),
+  computed: {
+    productClass(): string {
+      return this.product.stock ? 'success' : 'danger'
+    }
+  }
+})
 </script>
 
-<style></style>
+<style scoped>
+.success {
+  color: green;
+}
+
+.danger {
+  color: red;
+}
+</style>
