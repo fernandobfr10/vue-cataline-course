51087b78d69d896bf305991219717eeb7e35f73c
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 18:07:47 -0300

    Aula de v-for concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <ul>
+      <!-- Diretiva v-for é usada para criar loops e renderizar listas no html -->
+      <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    products: [
+      { id: 1, name: 'Camisa', stock: true },
+      { id: 2, name: 'Bota', stock: false },
+      { id: 3, name: 'Chapéu', stock: false },
+      { id: 4, name: 'Cinto', stock: true }
+    ]
+  })
+})
 </script>
 
 <style></style>
