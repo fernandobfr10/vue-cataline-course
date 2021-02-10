e426e7ec31e655aee06d93b157c9c007d9b07467
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 17:21:54 -0300

    Aula de v-html concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <!-- Irá imprimir a string normal, sem compilar para html -->
+    <p>Acesse: {{ link }}</p>
+
+    <!-- Assim como a diretiva v-text, o conteúdo da v-html ocupa o lugar do conteúdo da tag, dando erro se feito como abaixo -->
+    <!-- <p v-html="link">Acesse:</p> -->
+
+    <!-- Por isso, para dar certo neste caso, teremos que colocar o v-html em uma tag vazia, como abaixo -->
+    <p>Acesse: <span v-html="link" /></p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    link: '<a href="https://cataline.io">Cataline</a>'
+  })
+})
 </script>
 
 <style></style>
