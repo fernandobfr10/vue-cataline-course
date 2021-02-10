0cc4eb83abc6eaccbe7adf3d3cde271a32987a10
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 17:59:02 -0300

    Aula de v-show concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Hello World</h1>
+  <!-- É parecida com a V-if mas existem duas diferenças -->
+  <!-- 1 - Não tem encadeamento de opções como no v-if, ela simplesmente renderiza ou nào renderiza -->
+  <!-- 2 - A tag permanece na DOM, apenas é aplicado o display: none, já no v-if, não existe renderização da tag -->
+  <h1 v-show="sex === 'Masculino'">Masculino</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    sex: 'Masculino'
+  })
+})
 </script>
 
 <style></style>
