be2301d956245e63a38d3c422f6c157f10ca4997
Fernando Junior <fernando.junior85@outlook.com>
2021-02-11 15:23:29 -0300

    Transition - Aula Inicial Transição concluída

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,60 @@
 <template>
-  <h1>Hello World</h1>
+  <button @click="show = !show">Toggle</button>
+
+  <!-- O elemento que receberá a transition,deverá ficar envolvido na tag transition e toda a mágica deverá ser feita na tag style -->
+  <transition>
+    <h1 v-if="show">Cataline</h1>
+  </transition>
+
+  <!-- Quando existe mais de uma animação no componente, deve se dar nomes para manipular de forma correta cada uma delas -->
+  <transition name="fade">
+    <h1 v-if="show">Cataline</h1>
+  </transition>
+
+  <transition name="scale">
+    <h1 v-if="show">Cataline</h1>
+  </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  data: () => ({})
+  data: () => ({
+    show: false
+  })
 })
 </script>
 
-<style></style>
+<style>
+/* 
+v-enter-from (de onde esta vindo)
+v-enter-active (como vai acontecer)
+v-enter-to (para onde está indo)
+
+v-leave-from (de onde esta vindo)
+v-leave-active (como vai acontecer)
+v-leave-to (para onde está indo)
+
+O nome inicial `v` depende do atributo `name` - Quando trabalhamos com mais de uma animação, devemos dar nomes para elas exemplo do html
+*/
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+  scale: 0%;
+}
+
+.scale-enter-active,
+.scale-leave-active {
+  transition: scale 0.5s;
+}
+</style>
