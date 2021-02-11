704cbe81a695ae2c7d041436be0cd35896278b6c
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 21:49:47 -0300

    Classes Dinâmicas - Sintaxe de Objetos concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>Hello World</h1>
+  <!-- Criando classes baseando-se no estado do data -->
+  <!-- Adicionando a clase active quando o isActive for true, se for false, ela não será aplicada -->
+  <!-- Adicionando a clase text-danger quando a hasError for true, se for false, ela não será aplicada -->
+  <h1 class="static" :class="{ active: isActive, 'text-danger': hasError }">
+    Hello World
+  </h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data: () => ({
+    isActive: true,
+    hasError: false
+  })
+})
 </script>
 
 <style></style>
