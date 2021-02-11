f063c516a84f02fe9bea7675747317d714eea01d
Fernando Junior <fernando.junior85@outlook.com>
2021-02-10 22:11:31 -0300

    Componentização - Primeiro Componente concluída

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1>Hello World</h1>
+  <!-- Usando o componente ButtonStyled -->
+  <button-styled />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+// Importando o componente ButtonStyled
+import ButtonStyled from '@/components/ButtonStyled.vue'
 
 export default defineComponent({
+  // Registrando o componente ButtonStyled
+  components: { ButtonStyled },
   data: () => ({})
 })
 </script>
