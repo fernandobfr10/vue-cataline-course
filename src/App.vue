c8c133be7241bd4d4f04a4eafa4c0a64d966d0da
Fernando Junior <fernando.junior85@outlook.com>
2021-02-11 16:13:46 -0300

    Transition - Mode Concluída

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <button @click="show = !show">Toggle</button>
-  <!-- Usando o appear para iniciar a animação no carregamento da página,de acordo com o boolean do state show, dentro de data -->
+  <ul>
+    <li @click="view = 'Home'">Home</li>
+    <li @click="view = 'Contact'">Contato</li>
+    <li @click="view = 'About'">Sobre</li>
+  </ul>
+
+  <!-- A propriedade mode serve para ajudar na alternância de elementos -->
   <transition
-    enter-active-class="animate__animated animate__slideInLeft"
-    leave-active-class="animate__animated animate__slideInRight"
+    enter-active-class="animate__animated animate__fadeIn"
+    leave-active-class="animate__animated animate__fadeOut"
     :appear="show"
+    mode="out-in"
   >
-    <h1 v-if="show">Cataline</h1>
+    <component :is="view" />
   </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import 'animate.css'
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Contact from '@/pages/Contact.vue'
 
 export default defineComponent({
+  components: { Home, About, Contact },
   data: () => ({
-    show: true
+    view: 'Home'
   })
 })
 </script>
---
+++ src/mode.md
@@ -0,0 +1,7 @@
+Vue oferece alguns modos de transição alternativos:
+
+in-out
+Novo elemento realiza a transição de entrada primeiro; quando completada, o elemento atual realiza a transição de saída.
+
+out-in
+Elemento atual realiza a transição de saída primeiro; quando completada, o novo elemento realiza a transição de entrada.
