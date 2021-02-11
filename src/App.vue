9e44c02e05adca148c91426577ba5767bdc298a4
Fernando Junior <fernando.junior85@outlook.com>
2021-02-11 15:38:52 -0300

    Transition - Custom Transitions - Rename concluída

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <button @click="show = !show">Toggle</button>
-
-  <!-- O elemento que receberá a transition,deverá ficar envolvido na tag transition e toda a mágica deverá ser feita na tag style -->
-  <transition>
-    <h1 v-if="show">Cataline</h1>
-  </transition>
-
-  <!-- Quando existe mais de uma animação no componente, deve se dar nomes para manipular de forma correta cada uma delas -->
-  <transition name="fade">
-    <h1 v-if="show">Cataline</h1>
-  </transition>
-
-  <transition name="scale">
+  <!-- Alterando o nome padrão, de v-enter-active para entrada e de v-leave-active para saida -->
+  <transition
+    enter-active-class="animate__animated animate__slideInLeft"
+    leave-active-class="animate__animated animate__slideInRight"
+  >
     <h1 v-if="show">Cataline</h1>
   </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import 'animate.css'
 
 export default defineComponent({
   data: () => ({
@@ -27,34 +21,5 @@ export default defineComponent({
 </script>
 
 <style>
-/* 
-v-enter-from (de onde esta vindo)
-v-enter-active (como vai acontecer)
-v-enter-to (para onde está indo)
-
-v-leave-from (de onde esta vindo)
-v-leave-active (como vai acontecer)
-v-leave-to (para onde está indo)
-
-O nome inicial `v` depende do atributo `name` - Quando trabalhamos com mais de uma animação, devemos dar nomes para elas exemplo do html
-*/
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.scale-enter-from,
-.scale-leave-to {
-  scale: 0%;
-}
-
-.scale-enter-active,
-.scale-leave-active {
-  transition: scale 0.5s;
-}
+/* As classes estão vindo da biblioteca de animações css - animate.css */
 </style>
